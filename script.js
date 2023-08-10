775a0c7be577ef5c8cfd162c233d263f9efec036
Matej Sedlák <111086657+MatejS89@users.noreply.github.com>
2023-08-10 15:53:59 +0200

    Etch-A-Sketch: Refactor

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
-function main() {
-  const SIZE = 16;
-  let container = document.querySelector(".container");
-  for (let i = 0; i < SIZE ** 2; i++) {
+function createElements(size) {
+  let container = document.querySelector(".draw-container");
+  for (let i = 0; i < size ** 2; i++) {
     let element = document.createElement("div");
     element.classList.add("element");
-    element.style.flex = `1 1 ${500 / SIZE}px`;
+    element.style.flex = `1 1 ${500 / size}px`;
     container.appendChild(element);
   }
+}
 
+function createListeners() {
   let elements = document.querySelectorAll(".element");
-  elements.forEach((element) =>{
+  elements.forEach((element) => {
     element.addEventListener("mouseover", (elem) => {
       elem.currentTarget.classList.toggle("hover");
     });
     element.addEventListener("mouseout", (elem) => {
       elem.currentTarget.classList.toggle("hover");
     });
-    element.addEventListener("click",(elem) => {
+    element.addEventListener("click", (elem) => {
       elem.currentTarget.classList.toggle("selected");
-    })
-  }
-  );
+    });
+  });
+}
+
+function main() {
+  const SIZE = 16;
+  createElements(SIZE);
+  createListeners();
 }
 
 main();
